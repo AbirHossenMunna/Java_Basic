7caccd723e571b5b384529f8bd0b32df34ad186f
abirhossen422 <59090637+AbirHossenMunna@users.noreply.github.com>
2022-12-20 01:24:25 -0800

    update

--- src/java_class_2/FindGrade.java
+++ src/java_class_2/FindGrade.java
@@ -6,40 +6,91 @@ import java.util.Scanner;
 
 public class FindGrade {
     public static void main(String[] args) {
-        int marks[] = new int[6];
-        int i;
-        float total = 0, avg;
-        Scanner input = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        //Calculating GPA for the semester
+        System.out.println("How many courses did you take?");
+        int noOfCourses=sc.nextInt();
+        int totalCredit=0;
+        int courseMark;
+        double totalGpa=0;
 
-
-        for (i = 0; i < 6; i++) {
-            System.out.print("Enter Marks of Subject" + (i + 1) + ":");
-            marks[i] = input.nextInt();
-            total = total + marks[i];
+        for(int i=1;i<=noOfCourses ;i++){
+            System.out.println("What marks did you get in course "+i +"?");
+            courseMark=sc.nextInt();
+            String grade=findGrade((courseMark));
+            System.out.println("Grade for Course "+i+" : "+grade);
+            double courseGpa = course_gpa(grade);
+            System.out.println("GPA for course " + i +" : "+courseGpa);
+            System.out.println("What is the course credit?");
+            int course_credit=sc.nextInt();
+            totalGpa+=courseGpa*course_credit;
+            totalCredit+=course_credit;
         }
-        //Calculating average here
-        avg = total / 6;
-        System.out.print("The student Grade is: ");
-        if (avg >= 80) {
-            System.out.print("A+");
-        } else if (avg >= 75 && avg < 79) {
-            System.out.print("A");
-        } else if (avg >= 70 && avg < 74) {
-            System.out.print("A-");
-        }else if (avg >= 65 && avg < 69) {
-            System.out.print("B+");
-        }else if (avg >= 60 && avg < 64) {
-            System.out.print("B");
-        }else if (avg >= 55 && avg < 59) {
-            System.out.print("B-");
-        }else if (avg >= 50 && avg < 54) {
-            System.out.print("C+");
-        }else if (avg >= 45 && avg < 49) {
-            System.out.print("C");
-        }else if (avg >= 40 && avg < 44) {
-            System.out.print("D");
-        }else {
-            System.out.print("F");
+        double gpa=totalGpa/totalCredit;
+        System.out.println("CGPA for this semester is "+String.format("%.2f",gpa));
+
+    }
+    //finding grade by course mark
+    public static String findGrade(int mark){
+        String grade="";
+        if(mark>=90 && mark<=100){
+            grade="A";
+        }
+        else if(mark>=85 && mark<=89){
+            grade="A-";
+        }
+        else if(mark>=80 && mark<=84){
+            grade="B+";
+        }
+        else if(mark>=75 && mark<=79){
+            grade="B";
+        }
+        else if(mark>=70 && mark<=74){
+            grade="B-";
+        }
+        else if(mark>=65 && mark<=69){
+            grade="C+";
+        }
+        else if(mark>=60 && mark<=64){
+            grade="C";
+        }
+        else if(mark>=55 && mark<=59){
+            grade="D";
+        }
+        else{
+            grade="F";
+        }
+        return grade;
+
+    }
+    //finding GPA by grade
+    public static double course_gpa(String grade){
+        if (grade=="A"){
+            return 4.0;
+        }
+        else if(grade=="A-"){
+            return 3.7;
+        }
+        else if(grade=="B+"){
+            return 3.3;
+        }
+        else if(grade=="B"){
+            return 3.0;
+        }
+        else if(grade=="B-"){
+            return 2.7;
+        }
+        else if(grade=="C+"){
+            return 2.3;
+        }
+        else if(grade=="C"){
+            return 2.0;
+        }
+        else if(grade=="D"){
+            return 1.7;
+        }
+        else{
+            return 0;
         }
     }
 }
